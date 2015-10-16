91d35c4b646cd93d3547a358c3e86a7cc370e493
unknown <thomastran@ATC312-C03.business-cs.deanza.ad.fhda.edu>
2015-10-16 15:59:03 -0700

    Added cookie.png and Charizard can now eat it

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,64 @@
 #include <SFML/Graphics.hpp>
 #include "ResourcePath.h"
+#include "Overlap.h"
+
+void handleEvent(sf::RenderWindow& window)
+{
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			window.close();
+	}
+}
+
+void update(sf::Sprite& charizardSprite, sf::Sprite& cookieSprite, int& eaten)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		charizardSprite.move(0, -5);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		charizardSprite.move(0, 5);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		charizardSprite.move(-5, 0);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		charizardSprite.move(5, 0);
+	if (overlap(charizardSprite, cookieSprite))
+	{
+		eaten++;
+		charizardSprite.setScale(1.5, 1.5);
+	}
+}
+
+void draw(sf::RenderWindow& window, sf::Sprite& charizardSprite, sf::Sprite& cookieSprite, int& eaten)
+{
+	window.clear();
+	if (eaten == 0)
+	{
+		window.draw(cookieSprite);
+	}
+	window.draw(charizardSprite);
+	window.display();
+}
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works!");
+	window.setVerticalSyncEnabled(true);
+	sf::Texture charizardTexture;
+	charizardTexture.loadFromFile(resourcePath() + "assets/charizard.png");
+	sf::Sprite charizardSprite(charizardTexture);
+
+	sf::Texture cookieTexture;
+	cookieTexture.loadFromFile(resourcePath() + "assets/cookie.png");
+	sf::Sprite cookieSprite(cookieTexture);
+	cookieSprite.setPosition(400, 300);
+	int eaten = 0;
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
+		handleEvent(window);
+		update(charizardSprite, cookieSprite, eaten);
+		draw(window, charizardSprite, cookieSprite, eaten);
 	}
 
 	return 0;
